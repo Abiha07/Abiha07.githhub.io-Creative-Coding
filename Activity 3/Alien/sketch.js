1cd8e060009f90e0fbead680e068202a7a9741d9
Abiha07 <abiha0907.@gmail.com>
2025-06-02 10:24:16 +0400

    Add files via upload

---
+++ Activity 3/Alien/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+  createCanvas(500, 500);
+  background(0); 
+
+  fill(0, 250, 0); 
+  noStroke();
+  ellipse(200, 140, 120, 110);
+
+
+  fill(0);
+  ellipse(180, 130, 30, 40);
+  ellipse(220, 130, 30, 40);
+
+  stroke(100, 255, 0);
+  strokeWeight(5);
+  line(180, 80, 170, 60);
+  line(220, 80, 228, 60);
+
+  noStroke();
+  fill(250, 0, 110); 
+  ellipse(170, 60, 15, 15);
+  ellipse(230, 60, 15, 15);
+
+  fill(0, 255, 0);
+  ellipse(200, 230, 100, 120);
+
+  ellipse(150, 230, 30, 40);
+  ellipse(250, 230, 30, 40);
+
+  ellipse(180, 310, 40, 40);
+  ellipse(210, 310, 40, 40);
+}
